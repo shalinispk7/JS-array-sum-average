243fef1979c0fddfa471dd6c16d002151dda616e
shalinispk7 <shalinispk7@gmail.com>
2023-05-31 00:09:31 +0530

    sum and average of the given array

---
+++ main.js
@@ -0,0 +1,35 @@
+// sum of the elements in array
+let arr=[4,6,8];
+sum=0;
+document.write("The value store in array are: ");
+for (i=0;i<arr.length;i++){
+	sum=sum+arr[i];
+	document.write(arr[i]+" ");
+}
+	document.write("<br>");
+	document.write("Sum of all elements stored in the array is :"+sum+" ");
+	
+		document.write("<br>");
+		document.write("<br>");
+			
+			
+	// 3.average of the element in the array
+	let array=[4,6,8,9,22];
+sum=0;
+document.write("The value store in array are:");
+document.write("<br>");
+for (i=0;i<array.length;i++){
+	document.write(array[i]+" ");
+}
+document.write("<br>");
+for (i=0;i<array.length;i++){
+	sum=sum+array[i];
+	avg=sum/array.length;
+}
+	document.write("Sum of all elements stored in the array is :"+sum+" ");
+	document.write("<br>");
+	document.write("Average of elements in an array: "+avg);
+	
+
+	
+	
